5c0613e45f9c32f90c4861b8e86a074579386ae7
Juhi <shahjuhi1108@gmail.com>
2020-06-05 00:04:30 -0700

    navigate

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   }
   render() {
     return (
-      <Router>
+      <Router basename="/TweetyTweetProject/">
         <Fragment>
           <LoadingBar />
           <div className='container'>
